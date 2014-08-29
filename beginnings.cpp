efcfcf7fc2c1c46714f50f7442c4e8df711d34e2
waivek <waivek@gmail.com>
2014-08-29 22:26:03 +0530

    Global variables in cpp

--- beginnings.cpp
+++ beginnings.cpp
@@ -1,7 +1,8 @@
 using namespace std;
 #include <iostream>        // This is a key C++ library
 #include <cmath>           // The standard C library math.h
-
+double a = 1800;
+char str[100] = "Dragonriders born free to fly";
 int main ()
 {
 
@@ -14,11 +15,14 @@ int main ()
    //     cout<<"local b is "<<b<<endl;
    // }
    // cout<<"international b is "<<b<<endl;
-    int i = 900;
-    for(int i = 0; i < 10; i++) {
-        cout<<"The value of i is "<<i<<endl;
-    }
-    cout<<"The value of awesome i is "<<i<<endl;
+    // int i = 900;
+    // for(int i = 0; i < 10; i++) {
+    //     cout<<"The value of i is "<<i<<endl;
+    // }
+    // cout<<"The value of awesome i is "<<i<<endl;
+    cout<<"Global variable a is:"<<::a<<endl;
+    cout<<"Global string str is:"<<::str<<endl;
+    
     return 0;
 }
 
