e3537caeb9b17bb45b8d8e616d643f1762e566ee
waivek <waivek@gmail.com>
2014-08-29 19:53:42 +0530

    getting the hang of git yay

--- beginnings.cpp
+++ beginnings.cpp
@@ -4,12 +4,16 @@ using namespace std;
 
 int main ()
 {
-   double a;
 
-   a = 1.2;
-   a = sin (a);
 
-   cout << a << endl;
+   int b;
+   cout << "Enter the value of b\n";
+   cin >> b;
+   {
+       double b = 3.14;
+       cout<<"local b is "<<b<<endl;
+   }
+   cout<<"international b is "<<b<<endl;
 
    return 0;
 }
