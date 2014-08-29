93d7b1c4b0632bcfcfcc7648b7ce4f02091eacc7
waivek <waivek@gmail.com>
2014-08-29 19:41:29 +0530

    Added a new file

---
+++ beginnings.cpp
@@ -0,0 +1,17 @@
+using namespace std;
+#include <iostream>        // This is a key C++ library
+#include <cmath>           // The standard C library math.h
+
+int main ()
+{
+   double a;
+
+   a = 1.2;
+   a = sin (a);
+
+   cout << a << endl;
+
+   return 0;
+}
+
+
