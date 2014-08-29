8ff743fc2d5be4d29b2612171dcf733ecd991a88
waivek <waivek@gmail.com>
2014-08-29 20:01:11 +0530

    i can be initialized within the for loop

--- beginnings.cpp
+++ beginnings.cpp
@@ -6,16 +6,20 @@ int main ()
 {
 
 
-   int b;
-   cout << "Enter the value of b\n";
-   cin >> b;
-   {
-       double b = 3.14;
-       cout<<"local b is "<<b<<endl;
-   }
-   cout<<"international b is "<<b<<endl;
-
-   return 0;
+   // int b;
+   // cout << "Enter the value of b\n";
+   // cin >> b;
+   // {
+   //     double b = 3.14;
+   //     cout<<"local b is "<<b<<endl;
+   // }
+   // cout<<"international b is "<<b<<endl;
+    int i = 900;
+    for(int i = 0; i < 10; i++) {
+        cout<<"The value of i is "<<i<<endl;
+    }
+    cout<<"The value of awesome i is "<<i<<endl;
+    return 0;
 }
 
 
