6a3abedbb15f29f3fe4ee12e82cfc3f1ee9e6a8f
waivek <waivek@gmail.com>
2014-11-13 11:37:18 +0300

    BigInt, add has memory leak BigInt has ++, --, add(BigInt a, BigInt b) In add, memory leak because of pointer malloc which isn't free'd.

--- BigInt.cpp
+++ BigInt.cpp
@@ -21,8 +21,20 @@ public:
         ptr += MAX_SIZE - strlen(c) - 1;
         strcpy(ptr, c);
     }
+    // Line::Line(const Line &obj)
+// {
+//     cout << "Copy constructor allocating ptr." << endl;
+//     ptr = new int;
+//    *ptr = *obj.ptr; // copy the value
+// }
+    BigInt(const BigInt &obj) {
+        ptr = (char *) malloc(MAX_SIZE); 
+        memset(ptr, '0', MAX_SIZE);
+        ptr += MAX_SIZE - strlen(obj.ptr) -1;
+        strcpy(ptr, obj.ptr);
+    }
     ~BigInt() {
-        printf("Object is being deleted\n");
+        printf("BigInt: %s is being deleted\n", ptr);
         ptr += strlen(ptr) + 1 - MAX_SIZE;
         free(ptr);
     }
@@ -30,6 +42,11 @@ public:
     int mod(int a, int b);
     void increment(int c);
     void decrement(int c);
+    friend BigInt add(BigInt a, BigInt b);
+    BigInt operator++();
+    BigInt operator++(int);
+    BigInt operator--();
+    BigInt operator--(int);
 };
 
 void BigInt::display() 
@@ -48,25 +65,20 @@ int BigInt::mod(int a, int b)
 void BigInt::increment(int c = 0)
 {
     if(c == 0) {
-        if(strcmp(ptr, "1") == 0 && !positive) {
-            positive = 1;
-            decrement(1);
-            return;
-        }
         if(!positive) {
             decrement(1);
-            return;
+            if(*ptr == '0') {
+                positive = 1;
+            }
+            return ;
         }
     }
-    char *tmp = ptr + strlen(ptr) - 1;
-    while(1) {
-        *tmp = mod(*tmp - '0' + 1, 10)  + '0';
-        if(*tmp == '0') {
-            tmp--;
-        } else {
-            break;
-        }
-    }
+    char *tmp = ptr + strlen(ptr);
+    do {
+        tmp--;
+        *tmp = mod(*tmp - '0' + 1, 10) + '0';
+    } while(*tmp == '0');
+
     if(tmp < ptr) {
         ptr = tmp;
     }
@@ -82,42 +94,97 @@ void BigInt::decrement(int c = 0)
             return;
         }
     }
-    // printf("Decrement\n");
-    char *tmp = ptr + strlen(ptr) - 1;
-    while(1) {
-        // printf("While Loop\ntmp = %c\n", *tmp);
-        // int x = (*tmp - '0' - 1);
-        // *tmp = (x < 0 ? x+10:x) + '0';
+    char *tmp = ptr + strlen(ptr);
+    do {
+        tmp--;
         *tmp = mod(*tmp -'0' - 1, 10) + '0';
-        // printf("After modulo, tmp = %c\n", *tmp);
-        if(*tmp == '9') {
-            // printf("tmp == 9\n");
-            tmp--;
-        } else {
-            break;
-        }
-    }
-    if(*ptr == '0' /*&& *(ptr+1) != '\0'*/) {
-        // printf("ptr++\n");
+    } while(*tmp == '9');
+
+    if(*ptr == '0') {
         if(*(ptr+1) != '\0') {
             ptr++;
-        }/* else {
-            positive = mod(positive, 1);
-        }*/
+        }
     }
+}
+BigInt BigInt::operator++() 
+{
+    increment();
+    return BigInt(ptr);
+}
+BigInt BigInt::operator++(int) 
+{
+    BigInt B = ptr;
+    increment();
+    return B;
+}
+BigInt BigInt::operator--() 
+{
+    decrement();
+    return BigInt(ptr);
+}
+BigInt BigInt::operator--(int) 
+{
+    BigInt B = ptr;
+    decrement();
+    return B;
+}
+BigInt add(BigInt a, BigInt b) 
+{
+    int x = 0;
+    char *c;
+    c = (char *) malloc(a.MAX_SIZE);
+    memset(c, '0', a.MAX_SIZE);
+    c += a.MAX_SIZE  - 1;
+    // printf("\nc+1=%c\n", *(c+1));
+    *c = '\0';
+
+    char *p = a.ptr + strlen(a.ptr) - 1;
+    char *q = b.ptr + strlen(b.ptr) - 1;
+    // printf("\nx=%d\n", x++);
+
+    int carry = 0;
+
+    while((p >= a.ptr || q >= b.ptr) || carry != 0)
+    {
+        // printf("\nx=%d\n", x++);
+        c--;
+        int sum = (*p - '0') + (*q - '0') + carry;
+        carry = sum / 10;
+        sum = sum % 10;
+
+        // printf("\nx=%d\n", x++);
+        // printf("\nc=%c\n", *c);
+        *c = sum + '0';
+
+        // printf("\nx=%d\n", x++);
+        p--;
+        q--;
+    }
+    BigInt d = c;
+    // free(c);
+
+    return d;
 }
 int main(int argc, char * argv[])
 {
     char num[100] = "";
-    printf("Enter Big Integer:");
-    std::cin >> num;
-    BigInt b = num;
     char x = ' ';
     for(x = ' '; x != '?'; printf("? "), x = getchar()) {
         if(x == ' ') continue;
+        printf("a:");
+        std::cin >> num;
+        BigInt a = num;
+        printf("b:");
+        std::cin >> num;
+        BigInt b = num;
         // b.increment();
-        b.decrement();
-        b.display();
-
+        // b.decrement();
+        // b++;
+        // ++b;
+        // a.display();
+        // b.display();
+        BigInt c = add(a, b);
+        c.display();
+        getchar();
     }
 }
